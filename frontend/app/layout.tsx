69fea6e2e3da8da4b9db39b904334f6e41272698
alex <49459824+alexx-ngn@users.noreply.github.com>
2025-04-17 18:04:45 -0400

    Added PWA support

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -22,8 +22,14 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/favicon.ico" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="application-name" content="MilesForHope" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="MilesForHope" />
+        <meta name="mobile-web-app-capable" content="yes" />
       </head>
       <body className={inter.className}>
         <LanguageProvider>
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,3 +1,5 @@
+import withPWA from 'next-pwa'
+
 let userConfig = undefined
 try {
   // try to import ESM first
@@ -56,4 +58,9 @@ if (userConfig) {
   }
 }
 
-export default nextConfig
+export default withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})(nextConfig)
